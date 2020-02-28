31ac87d113e5da04090b35d352b0ab05a813c531
thoranderskow <tanderskow@gmail.com>
2020-02-27 22:42:19 -0600

    created

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function comment(){
+  var input = document.getElementById("comment1").value;
+  var p = document.getElementById("usercomment");
+  p.innerHTML = "<h3>&nbsp&nbsp&nbsp&nbspJumping_Jackrabbit</h3>  &nbsp&nbsp&nbsp&nbsp"+input;
+
+}
+
+function comment2(){
+  var input = document.getElementById("comment2").value;
+  var p = document.getElementById("usercomment2");
+  p.innerHTML = "<h3>&nbsp&nbsp&nbsp&nbspJumping_Jackrabbit</h3>  &nbsp&nbsp&nbsp&nbsp"+input;
+
+}
